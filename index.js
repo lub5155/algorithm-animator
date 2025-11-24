3e151c46f6faec0fb338e99068fec84c95b0ee27
lub5155 <vtarun2004@gmail.com>
2025-11-25 00:47:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const range=document.getElementById("rangeSlider")
 let arraysizedisp=document.getElementById("sizeValue")
 let rangesizedisp=document.getElementById("rangeValue")
 function sizedisp(){
-let arrsize=Math.random()*arraySize.value
+arraysizedisp.textContent=`${arraySize.value} elements`
 }
 function rangedisp(){
 rangesizedisp.textContent=`5- ${range.value}`
 }
 function randomArray(){
-console.log(document.getElementById("sizeSlider").value)
+let arrsize=Math.random()*arraySize.value
 }
