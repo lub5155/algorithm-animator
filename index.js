4d6a757385c6e8145b59af7281ba6a2b15fcd815
lub5155 <vtarun2004@gmail.com>
2025-11-25 15:46:00 +0530

    .

--- index.js
+++ index.js
@@ -15,4 +15,16 @@ for(let i=0;i<arraySize.value;i++){
 arrRand.push(Math.floor((Math.random()*(range.value-5))+5))
 }
 console.log(arrRand)
+displayArray()
+}
+function displayArray(){
+    const displayDiv = document.getElementById('display')
+    displayDiv.innerHTML = '' // Clear previous
+    arrRand.forEach(value => {
+        const bar = document.createElement('div')
+        bar.className = 'array-bar'
+        bar.style.height = `${value}px` // Or scale this value
+        displayDiv.appendChild(bar)
+    })
+    console.log(arrRand)
 }
